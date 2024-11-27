93649dced75314062b60be920753ba51098d4acd
Krrishna <codewithalpha@outlook.com>
2024-11-27 21:24:24 +0500

    add FOUR UP ASCII Art

--- main.asm
+++ main.asm
@@ -2,12 +2,22 @@ INCLUDE Irvine32.inc
 
 .data
 
+fourUp BYTE "                          ________   ________   ___  ___  ________        ___  ___  ________", 0ah                 
+       BYTE "                         |\  _____\ |\   __  \ |\  \|\  \|\   __  \      |\  \|\  \|\   __  \", 0ah  
+       BYTE "                          \ \  \__/ \ \  \|\  \\ \  \\\  \ \  \|\  \     \ \  \\\  \ \  \|\  \", 0ah 
+       BYTE "                           \ \   __\ \ \  \\\  \\ \  \\\  \ \   _  _\     \ \  \\\  \ \   ____\", 0ah
+       BYTE "                            \ \  \_|  \ \  \\\  \\ \  \\\  \ \  \\  \      \ \  \\\  \ \  \___|", 0ah
+       BYTE "                             \ \__\    \ \_______\\ \_______\ \__\\ _\      \ \_______\ \__\", 0ah                 
+       BYTE "                              \|__|     \|_______| \|_______||__|\|__|       \|_______||\|__|", 0ah  
+       BYTE " ", 0                                                                                                
+ 
+
 greet BYTE "Hello World!", 0
 
 .code
 main PROC
 
-mov edx, OFFSET greet
+mov edx, OFFSET fourUp
 call writestring
 
 
